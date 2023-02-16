50a1ee54f3586e782eb7acefbdc47438ff3e1599
Aaron Ragsdale <apragsdale@wisc.edu>
2023-02-16 14:21:49 -0600

    tackle reviews

--- fig2.py
+++ fig2.py
@@ -29,10 +29,11 @@ from util import *
 from bokeh.palettes import Colorblind as cb
 from bokeh.palettes import Bright
 
-#colors = cb[6]
+# colors = cb[6]
 colors = Bright[6]
 
 classes = ["A>C", "A>G", "A>T", "C>A", "C>G", "C>T"]
+
 
 def age_heat(fig, ax, ages1, ages2, dataset1=None, dataset2=None):
     min_age = 1e2
@@ -40,10 +41,14 @@ def age_heat(fig, ax, ages1, ages2, dataset1=None, dataset2=None):
     x = np.logspace(np.log10(min_age), np.log10(max_age), 200)
     ax.axline([0, 0], [1, 1], color="k", lw=0.1)
     cmap = "cubehelix_r"
-    #cmap = "gist_heat_r"
-    #cmap = "viridis"
+    # cmap = "gist_heat_r"
+    # cmap = "viridis"
     _, _, _, image = ax.hist2d(
-        ages1, ages2, bins=(x, x), cmap=cmap, cmin=1,
+        ages1,
+        ages2,
+        bins=(x, x),
+        cmap=cmap,
+        cmin=1,
     )
     image.set_edgecolor("face")
     ax.set_xscale("log")
@@ -54,12 +59,13 @@ def age_heat(fig, ax, ages1, ages2, dataset1=None, dataset2=None):
     ax.set_ylabel(f"Allele age ({dataset2})")
     ax.set_xticks([100, 1000, 10000, 100000])
     ax.set_yticks([100, 1000, 10000, 100000])
-    ax.set_aspect('equal')
+    ax.set_aspect("equal")
     r = np.corrcoef(ages1, ages2)[0][1]
     ax.text(20000, 200, rf"$r^2={r**2:0.2f}$", fontsize=5, va="center", ha="center")
     ax.xaxis.set_minor_locator(LogLocator(subs=(1.0,)))
     ax.yaxis.set_minor_locator(LogLocator(subs=(1.0,)))
     fig.colorbar(image, ax=ax)
+
 
 grid = (6, 5)
 fig = plt.figure(2, figsize=(6, 4.5))
@@ -101,12 +107,53 @@ age_heat(fig, ax3, ages1, ages2, dataset1="Relate", dataset2="tsdate")
 print("Plotted heat map for Relate and tsdate")
 
 # bar plots (shared + unique, and young vs trios)
-ax4 = plt.subplot2grid(grid, (0, 2), colspan=grid[1]-2, rowspan=3)
-ax5 = plt.subplot2grid(grid, (3, 2), colspan=grid[1]-2, rowspan=3)
+ax4 = plt.subplot2grid(grid, (0, 2), colspan=grid[1] - 2, rowspan=3)
+ax5 = plt.subplot2grid(grid, (3, 2), colspan=grid[1] - 2, rowspan=3)
 
 # shared and unique calls
 
-mutation_counts = {"shared": defaultdict(int), "geva": defaultdict(int), "relate": defaultdict(int)}
+mutation_counts = {
+    "shared": defaultdict(int),
+    "geva": defaultdict(int),
+    "relate": defaultdict(int),
+}
+
+# num_bs_per_chrom = {
+#    1: 20,
+#    2: 22,
+#    3: 19,
+#    4: 18,
+#    5: 17,
+#    6: 16,
+#    7: 14,
+#    8: 15,
+#    9: 11,
+#    10: 12,
+#    11: 12,
+#    12: 12,
+#    13: 9,
+#    14: 8,
+#    15: 7,
+#    16: 8,
+#    17: 7,
+#    18: 7,
+#    19: 4,
+#    20: 5,
+#    21: 3,
+#    22: 3,
+# }
+#
+# mutation_counts_bs = {
+#    i: {
+#        "shared": defaultdict(int),
+#        "geva": defaultdict(int),
+#        "relate": defaultdict(int),
+#    }
+#    for i in range(sum(num_bs_per_chrom.values()))
+# }
+#
+# bs_counter = 0
+print("counting shared and unique mutations")
 for chrom in range(22):
     ages = pd.read_pickle("data/ages.shared.chr{0}.geva-relate.pkl.gz".format(chrom))
     ages_geva = pd.read_pickle("data/ages.unique.chr{0}.geva.pkl.gz".format(chrom))
@@ -115,16 +162,71 @@ for chrom in range(22):
         mutation_counts["shared"][c] += len(ages[ages["Mut"] == c])
         mutation_counts["geva"][c] += len(ages_geva[ages_geva["Mut"] == c])
         mutation_counts["relate"][c] += len(ages_relate[ages_relate["Mut"] == c])
+    # num_sets = num_bs_per_chrom[chrom + 1]
+    # all_pos = np.concatenate((ages["Pos"], ages_geva["Pos"], ages_relate["Pos"]))
+    # all_pos = np.sort(all_pos)
+    # pos_break = []
+    # for i in range(num_sets):
+    #    pos_break.append(all_pos[i * 212500])
+    # pos_break.append(all_pos[-1] + 1)
+    # for i in range(num_sets):
+    #    for c in classes:
+    #        mutation_counts_bs[bs_counter]["shared"][c] += len(
+    #            ages[
+    #                (ages["Pos"] >= pos_break[i])
+    #                & (ages["Pos"] < pos_break[i + 1])
+    #                & (ages["Mut"] == c)
+    #            ]
+    #        )
+    #        mutation_counts_bs[bs_counter]["geva"][c] += len(
+    #            ages_geva[
+    #                (ages_geva["Pos"] >= pos_break[i])
+    #                & (ages_geva["Pos"] < pos_break[i + 1])
+    #                & (ages_geva["Mut"] == c)
+    #            ]
+    #        )
+    #        mutation_counts_bs[bs_counter]["relate"][c] += len(
+    #            ages_relate[
+    #                (ages_relate["Pos"] >= pos_break[i])
+    #                & (ages_relate["Pos"] < pos_break[i + 1])
+    #                & (ages_relate["Mut"] == c)
+    #            ]
+    #        )
+    #    bs_counter += 1
+    # print("finished chromosome", chrom + 1)
 
-totals = totals = {k: sum(v.values()) for k, v in mutation_counts.items()}
+totals = {k: sum(v.values()) for k, v in mutation_counts.items()}
+# muts_bs = []
+# totals_bs = []
+# for j in range(bs_counter):
+#    choices = np.random.choice(range(bs_counter), size=bs_counter)
+#    muts_bs.append(copy.deepcopy(mutation_counts_bs[choices[0]]))
+#    for choice in choices[1:]:
+#        for k in muts_bs[-1].keys():
+#            for c in muts_bs[-1][k].keys():
+#                muts_bs[-1][k][c] += mutation_counts_bs[choice][k][c]
+#    totals_bs.append({k: sum(v.values()) for k, v in muts_bs[-1].items()})
+
 props = {k: [v[c] / totals[k] for c in classes] for k, v in mutation_counts.items()}
+# props_bs = [
+#    {k: [v[c] / totals_bs[i][k] for c in classes] for k, v in muts_bs[i].items()}
+#    for i in range(bs_counter)
+# ]
+#
+# stderrs = {
+#    k: np.array([np.std([p[k][i] for p in props_bs]) for i in range(6)])
+#    for k in props.keys()
+# }
 
 width = 0.2
 x = np.arange(6)
 
-ax4.bar(x - width, props["shared"], 0.9*width, color=colors[0], label="Shared")
-ax4.bar(x, props["geva"], 0.9*width, color=colors[1], label="GEVA-unique")
-ax4.bar(x + width, props["relate"], 0.9*width, color=colors[2], label="Relate-unique")
+ax4.bar(x - width, props["shared"], 0.9 * width, color=colors[0], label="Shared")
+# ax4.errorbar(
+#    x - width, props["shared"], yerr=2 * stderrs["shared"], ecolor="k", elinewidth=1
+# )
+ax4.bar(x, props["geva"], 0.9 * width, color=colors[1], label="GEVA-unique")
+ax4.bar(x + width, props["relate"], 0.9 * width, color=colors[2], label="Relate-unique")
 
 ax4.legend(frameon=False, loc="upper right")
 ax4.set_xticks(x)
